60547c383cf29d4df50392994bde1980eb56fa5d
Jay Sloan <jay@Jays-MacBook-Air.local>
2023-03-08 13:45:49 +0200

    Finished up with todo section and added new option to tray

--- renderer/lib/components/TodoItem.tsx
+++ renderer/lib/components/TodoItem.tsx
@@ -11,36 +11,30 @@ type TodoItemProps = {
 export default function TodoItem(props: TodoItemProps) {
   return (
     <React.Fragment>
-      <div className="px-5 hover:cursor-pointer">
-        <div className="flex items-center gap-5">
-          <div className="h-6 w-6 border rounded-full p-1">
-            {props.completed ? (
-              <img src="../images/checkIcon.png" alt="" className="h-full w-full" />
-            ) : (
-              <div className="h-full w-full" />
-            )}
+      <div className="mx-20 hover:cursor-pointer">
+        <div className="pr-10">
+          <div>{props.title}</div>
+          <div className="truncate text-ellipsis overflow-hidden max-w-lg text-greyText">
+            {props.description}
           </div>
-          <div className="pr-10">
-            <div>{props.title}</div>
-            <div className="truncate text-ellipsis overflow-hidden max-w-lg text-greyText">
-              {props.description}
+          <div className="flex items-center gap-2 mt-1">
+            <div className="flex items-center gap-1">
+              <img
+                src="../images/calendarFine.png"
+                alt=""
+                className="h-4 w-4"
+              />
+              <div className="text-greyText">{props.dueDate}</div>
             </div>
-            <div className="flex items-center gap-2 mt-1">
-              <div className="flex items-center gap-1">
-                <img src="../images/calendarFine.png" alt="" className="h-4 w-4" />
-                <div className="text-greyText">{props.dueDate}</div>
-              </div>
-              <div className="rounded-full h-1 w-1 bg-gray-300" />
-              <div className="flex items-center gap-1">
-                <img src="../images/userIcon.png" alt="" className="h-4 w-4" />
-                <div className="text-greyText">{props.assignedTo}</div>
-              </div>
+            <div className="rounded-full h-1 w-1 bg-gray-300" />
+            <div className="flex items-center gap-1">
+              <img src="../images/userIcon.png" alt="" className="h-4 w-4" />
+              <div className="text-greyText">{props.assignedTo}</div>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="border border-gray-100 my-4 mx-6" />
     </React.Fragment>
   );
 }
--- renderer/lib/components/screens/TodoScreen.tsx
+++ renderer/lib/components/screens/TodoScreen.tsx
@@ -5,17 +5,26 @@ import supabase from "../../api/supabase_client";
 import TodoItem from "../TodoItem";
 
 export default function TodoScreen() {
+  const [todoLoading, setTodoLoading] = useState(false);
   const [loading, setLoading] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
   const [todos, setTodos] = useState<any[]>([]);
   const [todoIndex, setTodoIndex] = useState(null);
+  const [calendarShown, setCalendarShown] = useState(false);
 
-  const todoItems = Array.from({ length: 10 }, (_, i) => i);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [dueDateMonth, setDueDateMonth] = useState("");
+  const [dueDateDay, setDueDateDay] = useState("");
+  const [dueDateYear, setDueDateYear] = useState("");
+  const [assignedTo, setAssignedTo] = useState("");
+
+  const [newNote, setNewNote] = useState(false);
+  const [openAssignment, setOpenAssignment] = useState(false);
 
   useEffect(() => {
     loadTodos();
     console.log("Called");
-
     supabase
       .channel("any")
       .on(
@@ -32,7 +41,11 @@ export default function TodoScreen() {
   const loadTodos = async () => {
     try {
       setLoading(true);
-      const { data, error } = await supabase.from("Todos").select();
+      const { data, error } = await supabase
+        .from("Todos")
+        .select()
+        .order("completed", { ascending: true })
+        .order("due_date", { ascending: false });
 
       if (data) {
         setTodos(data);
@@ -74,7 +87,107 @@ export default function TodoScreen() {
   const handleOpenTodo = (index: number) => {
     setIsOpen(true);
     setTodoIndex(index);
+
+    setTitle(todos[index]?.title);
+    setDescription(todos[index]?.description);
+    setDueDateDay(formatDate(todos[index]?.due_date).split(" ")[0]);
+    setDueDateMonth(formatDate(todos[index]?.due_date).split(" ")[1]);
+    setDueDateYear(formatDate(todos[index]?.due_date).split(" ")[2]);
+    setAssignedTo(todos[index]?.assigned_to);
   };
+
+  const handleCloseModal = () => {
+    setIsOpen(false);
+    setCalendarShown(false);
+    setOpenAssignment(false);
+    setNewNote(false);
+    setAssignedTo("");
+  };
+
+  const editTask = async () => {
+    try {
+      setTodoLoading(true);
+      const { data, error } = await supabase
+        .from("Todos")
+        .update({
+          title,
+          description,
+          due_date: `${dueDateYear}-${dueDateMonth}-${dueDateDay}`,
+          assigned_to: assignedTo,
+        })
+        .eq("id", todos[todoIndex]?.id);
+    } catch (error) {
+      if (error instanceof Error) {
+        alert(error.message);
+      }
+    } finally {
+      setIsOpen(false);
+      // loadTodos();
+      setTodoLoading(false);
+    }
+  };
+
+  const setCompleted = async (index: number) => {
+    try {
+      // setLoading(true);
+      const { data, error } = await supabase
+        .from("Todos")
+        .update({ completed: todos[index].completed ? false : true })
+        .eq("id", todos[index]?.id);
+    } catch (error) {
+      if (error instanceof Error) {
+        alert(error.message);
+      }
+    } finally {
+      // loadTodos();
+      // setLoading(false);
+    }
+  };
+
+  const handleNewNote = async () => {
+    try {
+      setTodoLoading(true);
+      const { data, error } = await supabase.from("Todos").insert([
+        {
+          title,
+          description,
+          due_date: `${dueDateYear}-${dueDateMonth}-${dueDateDay}`,
+          assigned_to: assignedTo == "" ? "No one" : assignedTo,
+          completed: false,
+        },
+      ]);
+    } catch (error) {
+      if (error instanceof Error) {
+        alert(error.message);
+      }
+    } finally {
+      setIsOpen(false);
+      // loadTodos();
+      setTodoLoading(false);
+    }
+  };
+
+  const handleNewNoteOpen = () => {
+    const currentDay = new Date().getDate();
+    const currentMonth = new Date().getMonth() + 1;
+    const currentYear = new Date().getFullYear();
+
+    const day = currentDay < 10 ? `0${currentDay}` : currentDay;
+    const month = currentMonth < 10 ? `0${currentMonth}` : currentMonth;
+
+    setDueDateDay(day.toString());
+    setDueDateMonth(month.toString());
+    setDueDateYear(currentYear.toString());
+
+    setIsOpen(true);
+    setNewNote(true);
+  };
+
+  const handleSetAssignment = (name: string) => {
+    setAssignedTo(name);
+    setOpenAssignment(false);
+  }
+
 
   if (loading) {
     return (
@@ -96,9 +209,10 @@ export default function TodoScreen() {
             <Sheet.Content>
               <form className="p-5 flex flex-col text-greyText">
                 <input
-                  value={todos[todoIndex]?.title}
+                  defaultValue={newNote ? "" : todos[todoIndex]?.title}
                   autoFocus
                   type="text"
+                  onChange={(e) => setTitle(e.target.value)}
                   placeholder="Task name here..."
                   className="mb-3 focus:border-none focus:outline-none"
                 />
@@ -106,40 +220,128 @@ export default function TodoScreen() {
                 <textarea
                   name=""
                   id=""
+                  onChange={(e) => setDescription(e.target.value)}
                   placeholder="Description"
                   className="h-24 resize-none focus:border-none focus:outline-none"
-                  value={todos[todoIndex]?.description}
+                  defaultValue={newNote ? "" : todos[todoIndex]?.description}
                 ></textarea>
                 <div className="flex justify-between mt-4">
                   <div className="flex items-center gap-3">
-                    <div className="flex items-center text-greyText border py-1.5 gap-2 px-3 rounded-lg shadow-sm">
-                      <img
-                        src="../images/calendarFine.png"
-                        alt=""
-                        className="h-5 w-5"
-                      />
-                      <div>Due Date</div>
+                    <div
+                      onClick={() => setCalendarShown(true)}
+                      className="flex hover:cursor-pointer items-center text-greyText border py-1.5 gap-2 px-3 rounded-lg shadow-sm"
+                    >
+                      {calendarShown ? (
+                        <div className="flex gap-3 items-center">
+                          <img
+                            src="../images/calendarFine.png"
+                            alt=""
+                            className="h-5 w-5"
+                          />
+                          <div className="flex items-center">
+                            <input
+                              autoFocus
+                              type="text"
+                              defaultValue={
+                                newNote
+                                  ? dueDateDay
+                                  : todos[todoIndex]?.due_date.split("-")[2]
+                              }
+                              onChange={(e) => setDueDateDay(e.target.value)}
+                              size=""
+                              className="w-5 focus:border-none focus:outline-none"
+                              maxLength={2}
+                            />
+                            <div className="px-2">/</div>
+                            <input
+                              type="text"
+                              defaultValue={
+                                newNote
+                                  ? dueDateMonth
+                                  : todos[todoIndex]?.due_date.split("-")[1]
+                              }
+                              onChange={(e) => setDueDateMonth(e.target.value)}
+                              size=""
+                              className="w-5 focus:border-none focus:outline-none"
+                              maxLength={2}
+                            />
+                            <div className="px-2">/</div>
+                            <input
+                              type="text"
+                              defaultValue={
+                                newNote
+                                  ? dueDateYear
+                                  : todos[todoIndex]?.due_date.split("-")[0]
+                              }
+                              onChange={(e) => setDueDateYear(e.target.value)}
+                              size=""
+                              className="w-10 focus:border-none focus:outline-none"
+                              maxLength={4}
+                            />
+                          </div>
+                        </div>
+                      ) : (
+                        <>
+                          <img
+                            src="../images/calendarFine.png"
+                            alt=""
+                            className="h-5 w-5"
+                          />
+                          <div>Due Date</div>
+                        </>
+                      )}
                     </div>
 
-                    <div className="flex items-center text-greyText border py-1.5 px-3 gap-3 rounded-lg shadow-sm">
-                      <img
-                        src="../images/userIcon.png"
-                        alt=""
-                        className="h-5 w-5"
-                      />
-                      <div>Assign To</div>
+                    <div className="flex items-center relative text-greyText border py-1.5 px-3 gap-3 rounded-lg shadow-sm">
+                      {openAssignment ? (
+                        <div className="absolute -top-24 left-0 w-full bg-gray-300 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-200 shadow-sm p-2 flex flex-col">
+                          <div
+                            onClick={() => handleSetAssignment("Jay")}
+                            className="hover:cursor-pointer"
+                          >
+                            Jay
+                          </div>
+                          <div className="border my-2 border-gray-200 w-full" />
+                          <div
+                            onClick={() => handleSetAssignment("Jorene")}
+                            className="hover:cursor-pointer"
+                          >
+                            Jorene
+                          </div>
+                        </div>
+                      ) : null}
+                      <div
+                        onClick={() => openAssignment ? setOpenAssignment(false) : setOpenAssignment(true)}
+                        className="flex hover:cursor-pointer items-center gap-2"
+                      >
+                        <img
+                          src="../images/userIcon.png"
+                          alt=""
+                          className="h-5 w-5"
+                        />
+                        <div>{assignedTo == "" ? "Assign to" : assignedTo}</div>
+                      </div>
                     </div>
                   </div>
                   <div className="flex items-center gap-3">
                     <div
-                      onClick={() => setIsOpen(false)}
+                      onClick={() => handleCloseModal()}
                       className="border py-1.5 px-6 rounded-lg hover:cursor-pointer shadow-sm"
                     >
                       Cancel
                     </div>
-                    <div className="bg-purpleAccent text-white py-1.5 px-6 rounded-lg hover:cursor-pointer shadow-sm">
-                      Add Task
-                    </div>
+                    {todoLoading ? (
+                      <div className="bg-purpleAccent text-white py-2.5 px-10 rounded-lg hover:cursor-pointer shadow-sm">
+                        <MoonLoader color="#ffffff" size={14} />
+                      </div>
+                    ) : (
+                      <div
+                        onClick={() => (newNote ? handleNewNote() : editTask())}
+                        className="bg-purpleAccent text-white py-1.5 px-6 rounded-lg hover:cursor-pointer shadow-sm"
+                      >
+                        {newNote ? "Create Task" : "Edit Task"}
+                      </div>
+                    )}
                   </div>
                 </div>
               </form>
@@ -149,33 +351,41 @@ export default function TodoScreen() {
         </Sheet>
         {todos.map(function (item, index) {
           return (
-            <div key={index} onClick={() => handleOpenTodo(index)}>
-              <TodoItem
-                key={index}
-                title={item.title}
-                description={item.description}
-                completed={item.completed}
-                assignedTo={item.assigned_to}
-                dueDate={formatDate(item.due_date)}
-              />
+            <div key={index} className="relative">
+              <div
+                onClick={() => setCompleted(index)}
+                className="h-6 hover:cursor-pointer top-7 left-7 absolute w-6 border rounded-full p-1"
+              >
+                {todos[index].completed ? (
+                  <img
+                    src="../images/checkIcon.png"
+                    alt=""
+                    className="h-full w-full"
+                  />
+                ) : (
+                  <div className="h-full w-full" />
+                )}
+              </div>
+              <div onClick={() => (isOpen ? null : handleOpenTodo(index))}>
+                <TodoItem
+                  key={index}
+                  title={item.title}
+                  description={item.description}
+                  completed={item.completed}
+                  assignedTo={item.assigned_to}
+                  dueDate={formatDate(item.due_date)}
+                />
+                <div className="border border-gray-100 my-4 mx-6" />
+              </div>
             </div>
           );
         })}
-
-        {/* {todoItems.map(function (item, index) {
-          return (
-            <div key={index} onClick={() => setIsOpen(true)}>
-              <TodoItem
-                key={index}
-                title="Task Name"
-                description="Description"
-                completed={false}
-                assignedTo="Jay"
-                dueDate={formatDate("01-01-2021")}
-              />
-            </div>
-          );
-        })} */}
+        <div
+          onClick={() => handleNewNoteOpen()}
+          className="absolute top-5 right-5 hover:cursor-pointer"
+        >
+          <img src="/images/newNoteIcon.png" alt="" className="h-7 w-7" />
+        </div>
       </React.Fragment>
     );
   }
