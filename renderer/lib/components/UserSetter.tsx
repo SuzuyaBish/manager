bd48c0ca384cf670f20a50ed06d1706da51df043
Jay Sloan <jay@Jays-MacBook-Air.local>
2023-03-15 12:05:02 +0200

    Added user detection

--- renderer/lib/components/UserSetter.tsx
+++
@@ -1,7 +0,0 @@
-export default function UserSetter() {
-  return (
-    <div className="flex w-[800px] items-center justify-center">
-      <input type="text" placeholder="Your name" autoFocus className="placeholder:text-3xl w-fit text-3xl" />
-    </div>
-  );
-}
--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -1,5 +1,6 @@
-import Head from "next/head";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { MoonLoader } from "react-spinners";
+import supabase from "../lib/api/supabase_client";
 import GroceriesScreen from "../lib/components/screens/GroceriesScreen";
 import NotificationsScreen from "../lib/components/screens/NotificationsScreen";
 import TodoScreen from "../lib/components/screens/TodoScreen";
@@ -8,10 +9,47 @@ function Home() {
   const [todosClicked, setTodosClicked] = useState(true);
   const [notificationsClicked, setNotificationsClicked] = useState(false);
   const [groceriesClicked, setGroceriesClicked] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [userStatus, setUserStatus] = useState(null);
 
   const Store = require("electron-store");
-
   const store = new Store();
+
+  useEffect(() => {
+    if (store.get("user") == undefined || null) {
+      getUser();
+
+      if (!userStatus) {
+        store.set("user", "Jorene");
+      } else {
+        store.set("user", "Jay");
+      }
+    }
+    // store.delete("user")
+  }, []);
+
+  console.log(store.get("user"));
+
+  const getUser = async () => {
+    try {
+      setLoading(true);
+      const { data, error } = await supabase
+        .from("User")
+        .select("active")
+        .eq("user", "Jay");
+
+      if (data) {
+        setUserStatus(data[0].active);
+      }
+      if (error) throw error;
+    } catch (error) {
+      if (error instanceof Error) {
+        alert(error.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
 
   const handleClicked = (index: number) => {
     if (index === 0) {
@@ -40,53 +78,63 @@ function Home() {
       return <NotificationsScreen />;
     }
   };
-    
-  return (
-    <React.Fragment>
-      <div className="font-golos flex flex-col relative">
-        <div className="flex w-full gap-10 text-gray-500 py-5 text-xl">
-          <div className="absolute border-gray-100 border w-full top-[4.25rem]" />
-          <div
-            onClick={() => handleClicked(0)}
-            className="flex flex-col hover:cursor-pointer pl-5"
-          >
-            <div>Todo</div>
-            {todosClicked ? (
-              <div className="w-full border-[1px] border-purpleAccent z-10 mt-5" />
-            ) : (
-              <div></div>
-            )}
-          </div>
 
-          <div
-            onClick={() => handleClicked(1)}
-            className="flex flex-col hover:cursor-pointer"
-          >
-            <div>Groceries</div>
-            {groceriesClicked ? (
-              <div className="w-full border-[1px] border-purpleAccent z-10 mt-5" />
-            ) : (
-              <div></div>
-            )}
-          </div>
-
-          <div
-            onClick={() => handleClicked(2)}
-            className="flex flex-col hover:cursor-pointer"
-          >
-            <div>Notifications</div>
-            {notificationsClicked ? (
-              <div className="w-full border-[1px] border-purpleAccent z-10 mt-5" />
-            ) : (
-              <div></div>
-            )}
-          </div>
+  if (loading) {
+    return (
+      <React.Fragment>
+        <div className="pb-32 min-h-screen flex items-center justify-center">
+          <MoonLoader color="#672ee3" size={60} />
         </div>
+      </React.Fragment>
+    );
+  } else {
+    return (
+      <React.Fragment>
+        <div className="font-golos flex flex-col relative">
+          <div className="flex w-full gap-10 text-gray-500 py-5 text-xl">
+            <div className="absolute border-gray-100 border w-full top-[4.25rem]" />
+            <div
+              onClick={() => handleClicked(0)}
+              className="flex flex-col hover:cursor-pointer pl-5"
+            >
+              <div>Todo</div>
+              {todosClicked ? (
+                <div className="w-full border-[1px] border-purpleAccent z-10 mt-5" />
+              ) : (
+                <div></div>
+              )}
+            </div>
 
-        {whichPage()}
-      </div>
-    </React.Fragment>
-  );
+            <div
+              onClick={() => handleClicked(1)}
+              className="flex flex-col hover:cursor-pointer"
+            >
+              <div>Groceries</div>
+              {groceriesClicked ? (
+                <div className="w-full border-[1px] border-purpleAccent z-10 mt-5" />
+              ) : (
+                <div></div>
+              )}
+            </div>
+
+            <div
+              onClick={() => handleClicked(2)}
+              className="flex flex-col hover:cursor-pointer"
+            >
+              <div>Notifications</div>
+              {notificationsClicked ? (
+                <div className="w-full border-[1px] border-purpleAccent z-10 mt-5" />
+              ) : (
+                <div></div>
+              )}
+            </div>
+          </div>
+
+          {whichPage()}
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Home;
